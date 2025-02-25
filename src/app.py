b883cc7327a3a6546182c6be33c4c04cc2a08dfa
unknown <walaa.ai.shaaban@gmail.com>
2025-02-25 14:32:13 +0300

    change structue

---
+++ src/app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+from preprocessing_data import PreprocessingData
+
+def main():
+    st.title("📈 Stock Market Prediction News")
+    st.header("Analysis of financial news on equities to improve decision-making")
+    st.text("Stay updated with market trends and insights!")
+
+    # Adding a text input for user search
+    query = st.text_input("Enter a stock date between (2007-07-07 , 2024-07-08) ", "")
+
+    # Adding a button to trigger analysis
+    if st.button("The most important news for investment"):
+        st.write(f"Fetching and analyzing news for: **{query}**")
+        # You can integrate an API call or data processing here
+
+    # Adding a sidebar
+    st.sidebar.title("Navigation")
+    st.sidebar.write("🔍 Search news")
+    st.sidebar.write("📊 Stock trends")
+    st.sidebar.write("📉 Market sentiment")
+
+    preprocessing_data = PreprocessingData()
+    preprocessing_data.read_data()
+    preprocessing_data.save_data()
+
+if __name__ == "__main__":
+    main()
